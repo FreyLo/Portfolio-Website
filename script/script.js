eb7286fa547cf3c6c26d859d31f9ff509908a3d1
Loïc Frey <freyl2@bfh.ch>
2022-12-20 13:32:20 +0100

    Hochladen Webseite

---
+++ script/script.js
@@ -0,0 +1,12 @@
+// Sobald gescrollt wird, wird die Klasse navbarDark erstellt
+const header = document.querySelector('.navbar');
+
+window.onscroll = function() {
+    var top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
